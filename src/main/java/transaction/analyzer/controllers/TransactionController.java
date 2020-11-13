4877ec110ec2d92f65d34edbbee715cfcd0eb2de
Stanisław Nagórski <stanislaw.nagorski@gmail.com>
2020-11-13 22:30:45 +0100

    by department implanted

--- src/main/java/transaction/analyzer/controllers/TransactionController.java
+++ src/main/java/transaction/analyzer/controllers/TransactionController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import transaction.analyzer.models.Transaction;
 import transaction.analyzer.service.TransactionService;
-import transaction.analyzer.utils.EndPointsUtils;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,19 +25,25 @@ public class TransactionController {
         return service.findAll();
     }
 
-    @GetMapping("/average/{department}")
+    @GetMapping("/{department}/average")
     public String getAverageForDepartment(@PathVariable String department){
         double averageByDepartment = service.getAverageAmountByDepartment(department);
         return String.format(AVERAGE_INFO,department,averageByDepartment);
     }
 
-    @GetMapping("/max/{department}")
+    @GetMapping("/{department}/median")
+    public String getMedianForDepartment(@PathVariable String department){
+        double median = service.getMedianAmountByDepartment(department);
+        return String.format(MEDIAN_INFO, department, median);
+    }
+
+    @GetMapping("/{department}/max")
     public String getMaxForDepartment(@PathVariable String department){
         Optional<Transaction> maxAmountByDepartment = service.getMaxAmountByDepartment(department);
         return maxAmountByDepartment.isPresent()? maxAmountByDepartment.get().toString() : NOT_FIND;
     }
 
-    @GetMapping("/min/{department}")
+    @GetMapping("/{department}/min")
     public String getMinForDepartment(@PathVariable String department){
         Optional<Transaction> minAmountByDepartment = service.getMinAmountByDepartment(department);
         return minAmountByDepartment.isPresent()? minAmountByDepartment.get().toString() : NOT_FIND;
--- src/main/java/transaction/analyzer/service/TransactionService.java
+++ src/main/java/transaction/analyzer/service/TransactionService.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import static transaction.analyzer.utils.EndPointsUtils.DEPARTMENT_ALL;
 import static transaction.analyzer.utils.EndPointsUtils.convertToDouble;
 
 @Service
@@ -17,44 +18,55 @@ import static transaction.analyzer.utils.EndPointsUtils.convertToDouble;
 public class TransactionService {
     private final TransactionRepository repository;
 
-    public Optional<Transaction> findById(Long id){
+    public Optional<Transaction> findById(Long id) {
         return repository.findById(id);
     }
 
-    public List<Transaction> findAll(){
+    public List<Transaction> findAll() {
         return repository.findAll();
     }
 
-    public Transaction save(Transaction transaction){
+    public Transaction save(Transaction transaction) {
         return repository.save(transaction);
     }
 
-    public Iterable<Transaction> save(List<Transaction> transactions){
+    public Iterable<Transaction> save(List<Transaction> transactions) {
         return repository.saveAll(transactions);
     }
 
-    public double getAverageAmountByDepartment(String department){
-        return repository.findAllByDepartment(department).stream()
+    public double getAverageAmountByDepartment(String department) {
+        return getTransactions(department).stream()
                 .mapToDouble(value -> Double.parseDouble(value.getMoney().substring(1)))
                 .average().getAsDouble();
     }
 
-    public Optional<Transaction> getMaxAmountByDepartment(String department){
-        return repository.findAllByDepartment(department).stream()
+    public Optional<Transaction> getMaxAmountByDepartment(String department) {
+        return getTransactions(department).stream()
                 .max((o1, o2) -> (int) (convertToDouble(o1) - convertToDouble(o2)));
     }
-    public Optional<Transaction> getMinAmountByDepartment(String department){
-        return repository.findAllByDepartment(department).stream()
+
+    public Optional<Transaction> getMinAmountByDepartment(String department) {
+        return getTransactions(department).stream()
                 .min((o1, o2) -> (int) (convertToDouble(o1) - convertToDouble(o2)));
     }
 
-    public double getMedianAmountByDepartment(String department){
-        List<Double> doubles = repository.findAllByDepartment(department).stream()
+    public double getMedianAmountByDepartment(String department) {
+        List<Double> doubles = getTransactions(department).stream()
                 .map(EndPointsUtils::convertToDouble)
                 .collect(Collectors.toList());
         int size = doubles.size();
-        return size % 2 != 0 ? doubles.get(size/2) : doubles.get(size/2-1);
+        return size % 2 != 0 ? doubles.get(size / 2) : doubles.get(size / 2 - 1);
 
+    }
+
+    private List<Transaction> getTransactions(String department) {
+        List<Transaction> allByDepartment;
+        if (department.equals(DEPARTMENT_ALL)) {
+            allByDepartment = repository.findAll();
+        } else {
+            allByDepartment = repository.findAllByDepartment(department);
+        }
+        return allByDepartment;
     }
 
 }
